edf1d15151b83a6a4fdd44ef5245a8cc5844fcca
ni-kun <nicola.ferracin@gmail.com>
2016-01-18 17:46:51 +0100

    first commit

---
+++ public/js/controllers/HomeController.js
@@ -0,0 +1,20 @@
+app.controller('HomeController', function($scope, $http) {
+
+
+  // check if user logged in and set userLoggedIn boolean
+  // TODO uncomment if you need to manipulate user data back from the server
+  /*
+  loggedIn.getUser().then(
+    function(payload) {
+      if (payload.data) {
+        // TODO get the data you need from the User object stored in payload.data
+      } else {
+        console.log("The user is logged out")
+      }
+    },
+    function(errorPayload) {
+      console.log("Error: " + errorPayload)
+    });
+    */
+
+});
---
+++ public/js/controllers/IndexController.js
@@ -0,0 +1,45 @@
+app.controller('IndexController', function($scope, $http, loggedIn, $location) {
+
+  atPageLoad(loggedIn);
+
+
+
+  $scope.logout = function() {
+    // user is logging out
+    $http.get("/logout")
+    .success(function() {
+      $scope.userLoggedIn = false;
+      var privateLocations = []; // include any private route as "privateView"
+      if (privateLocations.indexOf(window.location.pathname) > -1) {
+        $location.path("/login");
+      }
+    })
+    .error(function(err) {
+      console.log("An error occured: " + err);
+    })
+  }
+});
+
+
+// work to do at page loading
+function atPageLoad(loggedIn) {
+  // check if user logged in and set userLoggedIn boolean
+  loggedIn.getUser().then(
+    function(payload) {
+      if (payload.data) {
+        $scope.userLoggedIn = true;
+        $scope.email = payload.data.local.email;
+      } else {
+        console.log("The user is not logged in")
+      }
+    },
+    function(errorPayload) {
+      console.log("Error: " + errorPayload)
+    });
+
+  // show a random background
+  var url = "./img/bg_" + Math.floor((Math.random() * 10) + 1) + ".jpg";
+  $('body').css("background-image", "url(" + url + ")");
+
+
+  }
