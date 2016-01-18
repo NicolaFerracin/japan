a5bb2eb1902792e40029060110efe05c8bef1f59
ni-kun <nicola.ferracin@gmail.com>
2016-01-18 17:48:15 +0100

    first commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// the database url to connect
-	url : 'mongodb://user:password@ds041150.mongolab.com:41150/heroku_9b1jl7rz'
+	url : 'mongodb://user:password@ds047325.mongolab.com:47325/japan'
 }
